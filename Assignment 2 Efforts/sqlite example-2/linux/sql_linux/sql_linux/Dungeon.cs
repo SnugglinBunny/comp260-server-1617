4d83c4aca5924f3b65419435489127d4180df864
John Rock <rasta_panda@hotmail.co.uk>
2018-05-06 12:42:08 +0100

    Hard coded the db location

--- Assignment 2 Efforts/sqlite example-2/linux/sql_linux/sql_linux/Dungeon.cs
+++ Assignment 2 Efforts/sqlite example-2/linux/sql_linux/sql_linux/Dungeon.cs
@@ -23,7 +23,7 @@ namespace Server
     public class Dungeon
     {
         sqliteConnection conn = null;
-        string databaseName = "./MUDData.database"; // Creates a string with the database name
+        string databaseName = "./home/JR191353/MUDData.database"; // Creates a string with the database name
 
         string currentRoom;
 
